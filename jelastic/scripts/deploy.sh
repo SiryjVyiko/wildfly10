d5be394c0f75764751a9b6c3cf6afc599a5418de
SiryjVyiko <dz@hivext.net>
2016-04-09 15:38:00 +0300

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Simple deploy and undeploy scenarios for JBoss7
+# Simple deploy and undeploy scenarios for Wildfly10
 
 WGET=$(which wget);
 
@@ -10,11 +10,15 @@ function _deploy(){
      [ -f "${WEBROOT}/${context}.ear" ] && rm -f ${WEBROOT}/${context}.ear;
      [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
      [ -f "${WEBROOT}/${context}.ear.undeployed" ] && mv ${WEBROOT}/${context}.ear.undeployed ${WEBROOT}/${context}.ear.deployed
-     $WGET --no-check-certificate --content-disposition "$package_url";
+     local download_dir=$(mktemp -d)
+     $WGET --no-check-certificate --content-disposition -P "$download_dir" "$package_url";
+     local app_filename=$(ls "download_dir")
+     [[ "$app_filename" =~ (.*.ear) ]] && mv -f "/${download_dir}/${app_filename}" "${WEBROOT}/${context}.ear" || "/${download_dir}/${app_filename}" "${WEBROOT}/${context}.war"
+     rm -rf "$download_dir"
 }
 
 function _undeploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
-     rm -f "${WEBROOT}/${context}.war" "${WEBROOT}/${context}.war.*" ;
-     rm -f "${WEBROOT}/${context}.ear" "${WEBROOT}/${context}.ear.*" ;
+     rm -f "${WEBROOT}/${context}.war" "${WEBROOT}/${context}.war.*" 1>/dev/null;
+     rm -f "${WEBROOT}/${context}.ear" "${WEBROOT}/${context}.ear.*" 1>/dev/null;
 }
