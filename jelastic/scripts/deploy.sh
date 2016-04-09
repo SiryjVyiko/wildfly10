7f02e2a0288939a8910e4aadec6c390cf411ab64
SiryjVyiko <dz@hivext.net>
2016-04-09 14:04:10 +0300

    Update deploy.sh

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -7,11 +7,18 @@ WGET=$(which wget);
 function _deploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] && rm -f ${WEBROOT}/${context}.war;
+     [ -f "${WEBROOT}/${context}.ear" ] && rm -f ${WEBROOT}/${context}.ear;
      [ -f "${WEBROOT}/${context}.war.undeployed" ] && mv ${WEBROOT}/${context}.war.undeployed ${WEBROOT}/${context}.war.deployed
-     $WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
+     [ -f "${WEBROOT}/${context}.ear.undeployed" ] && mv ${WEBROOT}/${context}.ear.undeployed ${WEBROOT}/${context}.ear.deployed
+     if [[ "$package_url" =~ (.*.ear) ]]; then
+	$WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.ear" "$package_url";
+     else
+     	$WGET --no-check-certificate --content-disposition -O "${WEBROOT}/${context}.war" "$package_url";
+     fi
 }
 
 function _undeploy(){
      [ "x${context}" == "xroot" ] && context="ROOT";
      [ -f "${WEBROOT}/${context}.war" ] &&  { rm -f "${WEBROOT}/${context}.war"; rm -f "${WEBROOT}/${context}.war.*" ; };
+     [ -f "${WEBROOT}/${context}.ear" ] &&  { rm -f "${WEBROOT}/${context}.ear"; rm -f "${WEBROOT}/${context}.ear.*" ; };
 }
